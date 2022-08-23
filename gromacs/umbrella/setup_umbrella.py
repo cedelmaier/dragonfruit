036910492196516f79b56bbce5bcff396c88eac6
Christopher Edelmaier <christopher.edelmaier@gmail.com>
2022-08-23 12:17:59 -0400

    updated umbrella sampling for specific use on rusty

--- gromacs/umbrella/setup_umbrella.py
+++ gromacs/umbrella/setup_umbrella.py
@@ -22,18 +22,6 @@ def parse_args():
     parser.add_argument('-i', '--interval', required = True, type = np.float32,
             help = 'Sampling interval (nm)')
 
-    # Threading and GPU settings
-    parser.add_argument('--ntmpi', type=int, default=4,
-            help='Number of MPI tasks')
-    parser.add_argument('--ntomp', type=int, default=20,
-            help='Number of OMP threads per MPI task')
-    parser.add_argument('--ngpu', type=int, default=4,
-            help='Number of GPUs to use')
-
-    # Input timing information
-    parser.add_argument('--nsday', type=float, default=50.0,
-            help='ns/day throughput')
-
     opts = parser.parse_args()
     return opts
 
@@ -52,7 +40,6 @@ class UmbrellaSampler(object):
         # Read the distance file
         self.distance_table = self.readDistanceFile(self.opts.distance)
         self.sample_interval = self.opts.interval
-
 
     def ProgOpts(self):
         r""" Run the program options
@@ -115,15 +102,6 @@ class UmbrellaSampler(object):
     def generateOutputFiles(self):
         r""" Generate output files for ourselves for the umbrella sampling
         """
-        # Get the information from opts for information numbers
-        ntmpi = np.int32(self.opts.ntmpi)
-        ntomp = np.int32(self.opts.ntomp)
-        ngpu = np.int32(self.opts.ngpu)
-
-        nsday = np.float32(opts.nsday)
-        # Need a fake 10 runs (10ns) and then plus 2 things
-        total_time_hr = np.int32(np.ceil(np.float32(12)*24.0/nsday*1.2))
-
         # Loop through all the possible files for this
         fname_list = []
         for i in range(len(self.sampled_indices)):
@@ -137,7 +115,7 @@ class UmbrellaSampler(object):
 
             # Write to a bash script
             fname = 'run_umbrella_frame{}.sh'.format(frame)
-            fname_next = 'run_umbrella_frame{}.sh'.format(next_frame)
+            plumed_name = 'plumed_measure_frame{}.dat'.format(frame)
 
             # Setup to write to a single batch file and submit all at once, as they are independent
             fname_list.append(fname)
@@ -147,40 +125,51 @@ class UmbrellaSampler(object):
 #!/bin/bash
 
 # Comments for running on the GPU cluster
-#SBATCH --job-name=membrane_umbrella
-#SBATCH --partition=volta-gpu
-#SBATCH --gres=gpu:{}
-#SBATCH --qos=gpu_access
-#SBATCH --ntasks={}
-#SBATCH --cpus-per-task={}
-#SBATCH --mem=8G
-#SBATCH --time={}:00:00
-#SBATCH --output=umbrella_run-%j.log
-#SBATCH --constraint=rhel8
-
-unset OMP_NUM_THREADS
+#SBATCH --job-name=umbrella
+#SBATCH --partition=ccb
+#SBATCH --reservation=rocky8
+#SBATCH -N 10
+#SBATCH --ntasks-per-node=120
+#SBATCH --constraint=rome,ib
 
 module purge
-module load gcc/9.1.0
-module load cuda/11.4
-source /nas/longleaf/apps/gromacs/2021.5/avx_512-cuda11.4/bin/GMXRC.bash
-
-# Get the number of cores and the linux version/name
-lscpu
-uname -a
+module load modules/2.0-20220630
+module load openmpi/4.0.7
+module load gromacs/mpi-plumed-2021.4
+module load plumed/mpi-2.8.0
 
 # Setup for a single umbrella run
 # First, setup the NPT runs
-gmx_gpu grompp -f npt_umbrella_v1.mdp -c conf{} -p topol.top -r conf{}.gro -n index.ndx -o npt{}.tpr -maxwarn 1
-gmx_gpu mdrun -deffnm npt{} -ntmpi {} --ntomp {} -nb gpu -bonded gpu -pme gpu -npme 1
+mpirun -np 1 gmx_mpi grompp -f npt_umbrella_v1.mdp -c zconf{} -p topol.top -r zconf{}.gro -n index.ndx -o npt{}.tpr -maxwarn 1
+mpirun gmx_mpi mdrun -deffnm npt{}
 
 # Now run the actual simulations
-gmx_gpu grompp -f md_umbrella_v1.mdp -c npt{}.gro -t npt{}.cpt -p topol.top -r npt{}.gro -n index.ndx -o umbrella{}.tpr
-gmx_gpu mdrun -deffnm umbrella{} -ntmpi {} --ntomp {} -nb gpu --bonded gpu -pme gpu -npme 1
-'''.format(ngpu, ntmpi, ntomp, total_time_hr, frame, frame, frame, frame, ntmpi, ntomp, frame, frame, frame, frame, frame, ntmpi, ntomp))
+mpirun -np 1 gmx_mpi grompp -f md_umbrella_v1.mdp -c npt{}.gro -t npt{}.cpt -p topol.top -r npt{}.gro -n index.ndx -o umbrella{}.tpr
+mpirun gmx_mpi mdrun -plumed plumed_measure_frame{}.dat -deffnm umbrella{}
+'''.format(frame, frame, frame, frame, frame, frame, frame, frame, frame, frame))
 
                 st = os.stat(fname)
                 os.chmod(fname, st.st_mode | stat.S_IEXEC)
+
+            # Write a plumed data file for this particular thing
+            with open(plumed_name, 'w') as rsh:
+                rsh.write('''
+# vim:ft=plumed
+MOLINFO STRUCTURE=reference.pdb
+
+# Figure out the lipid and helix COM coordinates
+lipid_com: COM ATOMS=313-50236
+helix_com: COM ATOMS=1-312
+
+# Get the Z distance
+z_dist: DISTANCE ATOMS=lipid_com,helix_com COMPONENTS
+
+# Get the alpha value
+alpha: ALPHARMSD RESIDUES=1-18
+
+# Print to a file
+PRINT ARG=* FILE=colvar_measure_{}.dat STRIDE=100
+'''.format(frame))
 
         # Now, put everything into a single submission script
         with open('submit_all.sh', 'w') as rsh:
